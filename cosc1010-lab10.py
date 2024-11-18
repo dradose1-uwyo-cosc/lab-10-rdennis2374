0f4d2bb02a07710565b94b503641114f0d334ef8
rdennis2374 <rdennis2@uwyo.edu>
2024-11-18 19:33:12 +0000

    m

--- cosc1010-lab10.py
+++ cosc1010-lab10.py
@@ -3,7 +3,7 @@
 # Submission Date 11/18/2024
 # Lab 10
 # Lab Section: 14
-# Sources, people worked with, help given to: 
+# Sources, people worked with, help given to: Stack Overflow, Geeks for Geeks
 # your
 # comments
 # here
